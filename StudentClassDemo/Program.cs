295c5d8b2568b75e6f66e70167a0029b3a0df86a
DESKTOP-SVECMJF\BC <scorbitt@comcast.net>
2021-06-22 16:18:02 -0600

    Add project files.

---
+++ StudentClassDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StudentClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student molly = new Student();
+            //molly.Name = "Molly the Cat";
+            //Console.WriteLine(molly.Name);
+
+            //molly.idNumber = 43;
+            //Console.WriteLine(molly.IdNumber);
+
+            Student.DoStuff();
+
+            Student joe = new Student();
+            joe.SetName("Joe Mama");
+
+            string joesName = joe.GetName();
+            Console.WriteLine("Joe's name is {0}", joesName);
+
+            Student jane = new Student();
+            jane.SetName("Janey");
+            Console.WriteLine("Jane's name is {0}",jane.GetName());
+
+            jane.SetScore1(100);
+            jane.SetScore2(60);
+            Console.WriteLine("Jane's average score is {0}", jane.GetAverageScore());
+
+            jane.SetScore2(30);
+            Console.WriteLine("Jane's new average score is {0}", jane.GetAverageScore());
+
+            Console.WriteLine("Jane's high score is {0}", jane.GetHighScore());
+            jane.SetScore2(110);
+            Console.WriteLine("Jane's high score is {0}", jane.GetHighScore());
+
+
+        }
+    }
+}
---
+++ StudentClassDemo/Student.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClassDemo
+{
+    class Student
+    {
+        private string name;
+        private int idNumber;
+        //private double dNum;
+        private double score1;
+        private double score2;
+        private double averageScore;
+
+
+        public Student()
+        {
+
+        }
+
+        public static void DoStuff()
+        {
+            Student molly = new Student();
+            molly.name = "Molly";
+            molly.idNumber = 43;
+            //molly.dNum = 67.0;
+            Console.WriteLine("Molly: {0}", molly.name);
+            Console.WriteLine("Molly: {0}", molly.idNumber);
+            //Console.WriteLine("Molly: {0}", molly.dNum);
+
+            Student someStudent = new Student();
+            Console.WriteLine("someStudent: {0}", someStudent.name);
+            Console.WriteLine("someStudent: {0}", someStudent.idNumber);
+            //Console.WriteLine("someStudent: {0}", someStudent.dNum);
+        }
+
+
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string newName)
+        {
+            name = newName;
+        }
+
+        public int GetIdNumber()
+        {
+            return this.idNumber;
+        }
+
+        public void SetIdNumber(int newIdNumber)
+        {
+            this.idNumber = newIdNumber;
+        }
+
+        public double GetScore1()
+        {
+            return this.score1;
+        }
+
+        public void SetScore1(double newScore1)
+        {
+            this.score1 = newScore1;
+            UpdateAverageScore();
+        }
+
+        public double GetScore2()
+        {
+            return this.score2;
+        }
+
+        public void SetScore2(double newScore2)
+        {
+            this.score2 = newScore2;
+            UpdateAverageScore();
+        }
+
+        public double GetAverageScore()
+        {
+            return averageScore;
+        }
+
+        private void UpdateAverageScore()
+        {
+            averageScore = (score1 + score2) / 2;
+        }
+
+        public double GetHighScore()
+        {
+            if (score1 > score2)
+            {
+                return score1;
+            }
+            else
+            {
+                return score2;
+            }
+        }
+    }
+
+
+}
